ee95aebc74ae4e93f9817d5f9e8fa865303a5a96
MaartenSlebos <maarten.slebos@hotmail.com>
2025-03-30 12:10:30 +0200

    import task.js into index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "./styles.css";
 
+import Task from './task.js'; 
+
+const task1 = new Task('buy milk', 'go to the grocery store to buy raw milk', '15', true); 
+console.log(task1)
+task1.toggleComplete(); 
+console.log(task1)
+task1.toggleComplete(); 
+console.group(task1)
--- src/task.js
+++ src/task.js
@@ -1,8 +1,14 @@
-// factory function 
-
-const createTask = (tile, description, dueDate, priority) {
-
-    return {
-        
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title; 
+        this.description = description; 
+        this.dueDate = dueDate; // Expecting "YYYY-MM-DD" like "2025-04-15"
+        this.priority = priority; 
+        this.complete = false; 
+    }
+    toggleComplete() {
+        this.complete = !this.complete; 
     }
 }
+
+export default Task; 
